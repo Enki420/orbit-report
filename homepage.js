82751fc0fe39373fe2b534f0b51d7d2fed2928bc
Enki420 <84172591+Enki420@users.noreply.github.com>
2024-03-11 06:15:30 -0400

    Add files via upload

---
+++ homepage.js
@@ -0,0 +1,14 @@
+// HomePage.js
+import React from 'react';
+
+function HomePage() {
+  return (
+    <div>
+      <h1>Welcome to AerialAspectAnalytix</h1>
+      <p>Empowering your solar PV designs with advanced aerial insights.</p>
+    </div>
+  );
+}
+
+export default HomePage;
+
